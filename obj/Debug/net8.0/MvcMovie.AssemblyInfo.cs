8fe59d9565597a515c7cc36c342af04ce0c92a52
Ankit Lad-SujitOffice <ankit.utu@gmail.com>
2025-07-09 15:20:22 +0530

    add gitignore

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655c4c72179b10117f9badf54e2ce0c192119d48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67aa3219f44f70623333be67f39d27d3b9ed8bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
