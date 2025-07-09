f67aa3219f44f70623333be67f39d27d3b9ed8bb
Ankit Lad-SujitOffice <ankit.utu@gmail.com>
2025-07-09 15:13:23 +0530

    test

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac5407501376f72760f7cdf0583b63fbf7b33e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655c4c72179b10117f9badf54e2ce0c192119d48")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
